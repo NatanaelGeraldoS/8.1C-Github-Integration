cd04a2be11e74d9b97de23bb02ee5fbfef2e8890
Natanael Geraldo <geraldonatanael84@gmail.com>
2025-09-07 11:23:39 +1000

    feat: Add Student Information

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-// Test Changes
+const student = {
+  name: "Natanael Geraldo Sulaiman",
+  studentId: "2250000297",
+  email: "s2250000297@deakin.edu.au"
+};
+
+// Log details
+console.log("Student Information:");
+console.log("Name:", student.name);
+console.log("Student ID:", student.studentId);
+console.log("Email:", student.email);
